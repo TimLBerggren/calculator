f181c027ef5cb57c1d816670a5848b46f159d16b
Tim Berggren <125907043+TimLBerggren@users.noreply,github.com>
2024-08-21 22:29:26 +0200

    included the operators variable and assigned the symbols to the respective operation

--- calculator.js
+++ calculator.js
@@ -4,8 +4,15 @@
 
 // we want to have three variables that represent the operations: one variable the includes the first number, another that has the operator, and then a second number that appears after the operator 
 let initialNumber = ''
-let operator = ''
+let operation = ''
 let secondNumber = ''
+
+const operators = {
+    '+': 'plus',
+    '-': 'minus',
+    '*': 'multiply',
+    '/': 'divide',
+  };
 
 function add(a, b) {
     return a + b
@@ -21,4 +28,9 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b
+}
+
+// we want to create a function that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function operator(initialNumber, operation, secondNumber) {
+
 }
