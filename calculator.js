0e8877976a51a61123d3f9e861a6538ccab05329
Tim Berggren <125907043+TimLBerggren@users.noreply,github.com>
2024-08-21 22:01:40 +0200

    added three variables: initialNumber, operator, secondNumber

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+
+
+
+
+// we want to have three variables that represent the operations: one variable the includes the first number, another that has the operator, and then a second number that appears after the operator 
+let initialNumber = ''
+let operator = ''
+let secondNumber = ''
+
 function add(a, b) {
     return a + b
 }
