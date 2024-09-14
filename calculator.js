05118320efe958ee7d76934207936c0d64737525
Tim Berggren <tim.berggren@hotmail.com>
2024-09-14 15:44:23 +0200

    included code from other machine into the new one even though it is unfinished. It includes functions and switch cases that define the operations to conduct when number are clicked and equal sign is pressed

--- calculator.js
+++ calculator.js
@@ -6,67 +6,78 @@ let displayValue = '';
 const display = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator")
-const clearButton = document.querySelector(".clear");
+const equalButton = document.querySelector("#equal");
+const clearButton = document.querySelector("#clear");
 const deleteButton = document.querySelector("#delete");
 
-
-// const operators = {
-//     '^': 'power',
-//     '+': 'plus',
-//     '-': 'minus',
-//     '*': 'multiply',
-//     '/': 'divide'
-//   };
-
-const power = (a, b) => a ** b;
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => (b === 0) ? 'Cannot divide by 0' : a / b;
-
-const operate = (firstNumber, operation, secondNumber) => {
-  if (operation === 'power') {
-    return power(firstNumber, secondNumber);
-  } else if (operation === 'plus') {
-    return add(firstNumber, secondNumber);
-  } else if (operation === 'minus') {
-    return subtract(firstNumber, secondNumber);
-  } else if (operation === 'multiply') {
-    return multiply(firstNumber, secondNumber);
-  } else if (operation === 'divide') {
-    return divide(firstNumber, secondNumber);
-  }
-
-  return 'Invalid operation';
-}
+const power = (firstNumber, secondNumber) => Number(firstNumber) ** Number(secondNumber);
+const add = (firstNumber, secondNumber) => Number(firstNumber) + Number(secondNumber);
+const subtract = (firstNumber, secondNumber) => Number(firstNumber) - Number(secondNumber);
+const multiply = (firstNumber, secondNumber) => Number(firstNumber) * Number(secondNumber);
+const divide = (firstNumber, secondNumber) => (Number(secondNumber) === 0) ? 'Cannot divide by 0' : Number(firstNumber) / Number(secondNumber);
 
 const updateDisplay = (value) => {
-  if (displayValue === '' && value === '.') {
-    displayValue = '0.';
-  } else {
-    displayValue += value;
-  }
+  displayValue += value;
   display.textContent = displayValue;
 }
 
+equalButton.addEventListener("click", () => {
+  if (firstNumber !== '' && operation !== '' && secondNumber !== '') {
+    const a = parseFloat(firstNumber);
+    const b = parseFloat(secondNumber);
+    let result = '';
+
+    switch (operation) {
+      case "+":
+        result = add(a, b);
+        break;
+      case "-":
+        result = subtract(a, b);
+        break;
+      case "x":
+        result = multiply(a, b);
+        break;
+      case "÷":
+        result = divide(a, b);
+        break;
+      case "^":
+        result = power(a, b);
+        break;
+    }
+  displayValue = result.toString();
+  display.textContent = displayValue;
+
+  firstNumber = result.toString();
+  secondNumber = '';
+  operation = '';
+  }
+});
+
 numberButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        const value = e.target.textContent.trim();
-        if (displayValue === '0') {
-            displayValue = '';
-        }
-        updateDisplay(value);
-    });
+  button.addEventListener("click", (e) => {
+      const value = e.target.textContent.trim();
+
+      if (operation === '') {
+          firstNumber += value;
+      } else {
+        secondNumber += value;
+      }
+      updateDisplay(value);
+  });
 });
 
 operatorButtons.forEach(button => {
     button.addEventListener("click", (e) => {
       const value = e.target.textContent.trim();
-      if (displayValue === '0') {
-        displayValue = '';
-      }
-      updateDisplay(value);
-  })
+
+      if (operation !== '' && secondNumber === '')
+        return;
+
+      operation = value;
+
+      updateDisplay(`${operation}`);
+      displayValue = (`${firstNumber}${secondNumber}`);
+  });
 });
 
 clearButton.addEventListener("click", () => {
@@ -88,3 +99,40 @@ deleteButton.addEventListener("click", () => {
 
 //TODO
 // when a user inputs the second number, the operation should be executed and return the result
+
+
+// Other stuff
+// const operators = {
+//     '^': 'power',
+//     '+': 'plus',
+//     '-': 'minus',
+//     '*': 'multiply',
+//     '/': 'divide'
+//   };
+
+
+// const operate = (firstNumber, operation, secondNumber) => {
+//   if (operation === 'power') {
+//     return power(firstNumber, secondNumber);
+//   } else if (operation === 'plus') {
+//     return add(firstNumber, secondNumber);
+//   } else if (operation === 'minus') {
+//     return subtract(firstNumber, secondNumber);
+//   } else if (operation === 'multiply') {
+//     return multiply(firstNumber, secondNumber);
+//   } else if (operation === 'divide') {
+//     return divide(firstNumber, secondNumber);
+//   }
+
+//   return 'Invalid operation';
+// }
+
+
+// const updateDisplay = (value) => {
+//   if (displayValue === '' && value === '.') {
+//     displayValue = '0.';
+//   } else {
+//     displayValue += value;
+//   }
+//   display.textContent = displayValue;
+// }
